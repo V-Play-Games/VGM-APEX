2a8665222108b3acc54d342a5f52f54e252f2020
V Play Games <vaibhavnargwani28@gmail.com>
2025-06-07 15:15:00 +0530

    Remove XSearchItem components Why not completely eradicate them?

--- app/src/main/java/net/vpg/apex/ui/screens/SearchScreen.kt
+++ app/src/main/java/net/vpg/apex/ui/screens/SearchScreen.kt
@@ -1,14 +1,12 @@
 package net.vpg.apex.ui.screens
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.getValue
@@ -23,8 +21,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import net.vpg.apex.core.di.rememberSearchHistory
 import net.vpg.apex.entities.ApexTrack
-import net.vpg.apex.ui.components.search.CurrentSearchItem
-import net.vpg.apex.ui.components.search.RecentSearchItem
+import net.vpg.apex.ui.components.common.TrackBar
 
 object SearchScreen : ApexBottomBarScreen(
     route = "search",
@@ -95,7 +92,10 @@ object SearchScreen : ApexBottomBarScreen(
             } else {
                 LazyColumn {
                     items(searchResults) { track ->
-                        CurrentSearchItem(track)
+                        TrackBar(
+                            apexTrack = track,
+                            onClick = { searchHistory.addTrack(track) }
+                        )
                     }
                 }
             }
@@ -115,7 +115,19 @@ object SearchScreen : ApexBottomBarScreen(
             } else {
                 LazyColumn {
                     items(searchHistory.getTracks()) { track ->
-                        RecentSearchItem(track)
+                        TrackBar(
+                            apexTrack = track,
+                            trailingComponents = {
+                                Icon(
+                                    imageVector = Icons.Default.Close,
+                                    contentDescription = "Close",
+                                    modifier = Modifier
+                                        .size(40.dp)
+                                        .clickable { searchHistory.removeTrack(track) },
+                                    tint = Color.DarkGray
+                                )
+                            }
+                        )
                     }
                 }
             }
