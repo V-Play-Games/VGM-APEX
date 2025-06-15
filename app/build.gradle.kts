68c80b729c440ed861308001035adb49ecab9b6d
V Play Games <vaibhavnargwani28@gmail.com>
2025-06-15 20:31:20 +0530

    Release 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         minSdk = 29
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.0"
+        versionCode = 3
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
